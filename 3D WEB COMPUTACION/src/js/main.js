d63dc82bddba08ede29644b999ee20fa68169acb
Cantorr <macantorr@correo.usbcali.edu.co>
2023-09-01 17:41:32 -0500

    correciones aleatoreidad y rotacion

--- 3D WEB COMPUTACION/src/js/main.js
+++ 3D WEB COMPUTACION/src/js/main.js
@@ -1,17 +1,14 @@
-
 var scene = null,
     camera = null,
     renderer = null,
     cube = null,
     torus = null,
-    cone = null;
-controls = null;
-var shapes = [];
+    cone = null,
+    controls = null,
+    shapes = [];
+
 const size = 100,
     divisions = 100;
-
-
-
 
 function startScene() {
     //scene render
@@ -32,7 +29,8 @@ function startScene() {
     //------------------------ORBIT CONTROLS----------------------------------
 
     controls = new THREE.OrbitControls(camera, renderer.domElement);
-    camera.position.set(0, 0, 0);
+    camera.position.set(20, 20, 20);
+    camera.position.z = 20;
     controls.update();
     //GRID HELPER
     const gridHelper = new THREE.GridHelper(size, divisions);
@@ -45,56 +43,31 @@ function startScene() {
 
     //Animation
     animate();
-
-    // to delete
-    //-----------------------------OBJECTS-------------------------------------------
-    //cube
-
-
-    //const geometry = new THREE.BoxGeometry(10, 10, 10);
-    //const material = new THREE.MeshBasicMaterial({ color: 0xF5F8FA, wireframe : true });
-    //cube = new THREE.Mesh(geometry, material);
-    //scene.add(cube);
-    //cube.position.x=0;
-
-    //torus
-    //const geometrytorus = new THREE.TorusGeometry( 5.757, 1.2375, 22, 33, 6.283185307179586 ); 
-    //const materialtorus = new THREE.MeshBasicMaterial( { color: 0xF5F8FA, wireframe : true } ); 
-    //torus = new THREE.Mesh( geometrytorus, materialtorus );
-    //scene.add( torus );
-    //torus.position.x = -20;
-    //camera.position.z = 20;
-
-
-    //Cone
-    //const geometrycone = new THREE.ConeGeometry( 5, 20, 32 ); 
-    //const materialcone = new THREE.MeshBasicMaterial( {color: 0xF5F8FA , wireframe : true} );
-    //cone = new THREE.Mesh(geometrycone, materialcone ); 
-    //scene.add( cone );
-    //cone.position.x =  20;
-    //camera.position.z = 25;
-
-
-
 }
 
 
-//FUNCTION ANIMATE
+//FUNCTION ANIMATE (se ejecuta por cada frame, por eso los controles estan aqui)
 function animate() {
-    // renderer.createObjects(answer);
     requestAnimationFrame(animate);
     controls.update();
     renderer.render(scene, camera);
+    if (cube != null, cone != null, torus != null) {
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        cone.rotation.x -= 0.02;
+        cone.rotation.y -= 0.02;
+        torus.rotation.x -= 0.02;
+        torus.rotation.y -= 0.02;
+    }
 
+    // for 
+    // for 
     // for (let i = 0; i < figura.length; i++) {
 
     //     shapes[i].rotation.x += 0.01;
     //     shapes[i].rotation.z -= 0.01;
 
     // }
-
-
-
 }
 
 
@@ -110,22 +83,22 @@ function onWindowResize() {
 
 }
 
-function createObjects(answer) {
+function createObjects(objectType) {
 
-    switch (answer) {
+    switch (objectType) {
 
         case "Cube":
 
-            AnimateCube()
+            createCube()
 
             break
 
         case "Cone":
-            AnimateCone()
+            createCone()
             break
 
         case "Torus":
-            AnimateTorus()
+            createTorus()
             break
 
 
@@ -137,111 +110,91 @@ function createObjects(answer) {
 
 
 }
-function AnimateCube() {
-    requestAnimationFrame(AnimateCube);
-    const texture = new THREE.TextureLoader().load('../image/animales/face1.jpg' ); 
+function createCube() {
+    //const texture = new THREE.TextureLoader().load('../image/animales/face1.jpg');
 
-    var MaterialCube=[new THREE,MeshBasicMaterial ({map:texture})
-
-    
-    
-    ];
-const material = new THREE.MeshBasicMaterial( { map:texture } );
+    //var MaterialCube = [new THREE, MeshBasicMaterial({ map: texture })
 
 
-    geometry = new THREE.BoxGeometry(10, 10, 10);
-    material = new THREE.MeshStandardMaterial({
-        map: texture,
-        color: 0xF5F8FA,
-        wireframe: false,
-        color: 0xFAF7F7,
-        transparent: false,
-        opacity: 1
-    });
+
+    //];
+    //const material = new THREE.MeshBasicMaterial({ map: texture });
 
 
-    cube = new THREE.Mesh(geometry, material);
+    geometrycube = new THREE.BoxGeometry(10, 10, 10);
+    const materialcube = new THREE.MeshBasicMaterial({ color: 0xF5F8FA, wireframe: true }); //linea temporal para la "textura" del objeto, eliminala cunado aprendas a definir el material del objeto
+
+    //material = new THREE.MeshStandardMaterial({
+    //    map: texture,
+    //  color: 0xF5F8FA,
+    // wireframe: false,
+    //color: 0xFAF7F7,
+    //transparent: false,
+    //opacity: 1
+    //});
+
+
+    cube = new THREE.Mesh(geometrycube, materialcube);
     scene.add(cube);
     cube.position.x += (Math.random() - 0.4) * size;
     cube.position.z += (Math.random() - 0.4) * size;
-    camera.position.z = 20;
-    controls.update();
-    renderer.render(scene, camera);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-
-
-
 }
 
-function AnimateCone() {
-    requestAnimationFrame(AnimateCone);
+function createCone() {
     const geometrycone = new THREE.ConeGeometry(5, 20, 32);
-    const materialcone = new THREE.MeshBasicMaterial({ color: 0xF5F8FA, wireframe: false });
+    const materialcone = new THREE.MeshBasicMaterial({ color: 0xF5F8FA, wireframe: true });
     cone = new THREE.Mesh(geometrycone, materialcone);
     scene.add(cone);
     cone.position.x = (Math.random() - 0.4) * size;
     cone.position.z = (Math.random() - 0.4) * size;
-    camera.position.z = 20;
-    controls.update();
-    renderer.render(scene, camera);
-    cone.rotation.x += 0.01;
-    cone.rotation.y += 0.01;
-
 }
 
-function AnimateTorus() {
-    requestAnimationFrame(AnimateTorus);
+function createTorus() {
     const geometrytorus = new THREE.TorusGeometry(5.757, 1.2375, 22, 33, 6.283185307179586);
-    const materialtorus = new THREE.MeshBasicMaterial({ color: 0xF5F8FA, wireframe: false});
+    const materialtorus = new THREE.MeshBasicMaterial({ color: 0xF5F8FA, wireframe: true });
     torus = new THREE.Mesh(geometrytorus, materialtorus);
     scene.add(torus);
     torus.position.x = (Math.random() - 0.4) * size;
     torus.position.z = (Math.random() - 0.4) * size;
-    camera.position.z = 20;
     controls.update();
-    renderer.render(scene, camera);
-    torus.rotation.x -= 0.02;
-    torus.rotation.y -= 0.02;
 }
 
-function CreateLight (typeLight){
+function CreateLight(typeLight) {
 
-    switch (typeLight) { 
+    switch (typeLight) {
         case "Ambient":
-            const AmbientLight = new THREE.AmbientLight( 0x404040 ); // soft white light
-            scene.add( AmbientLight );
-         break;
-         
+            const AmbientLight = new THREE.AmbientLight(0x404040); // soft white light
+            scene.add(AmbientLight);
+            break;
+
         case "PointLight":
-             // Create Point Light
-         const Pointlight = new THREE.PointLight(0xfffff, 10, 100);
-         Pointlight.position.set(10, 10, 10);
-         scene.add(Pointlight);
-         break;
-        
-        
-         
-        case "SpotLight" :
-            const spotLight = new THREE.SpotLight( 0xffffff );
-            spotLight.position.set( 100, 1000, 100 );
-            spotLight.map = new THREE.TextureLoader().load( url );
-            
+            // Create Point Light
+            const Pointlight = new THREE.PointLight(0xfffff, 10, 100);
+            Pointlight.position.set(10, 10, 10);
+            scene.add(Pointlight);
+            break;
+
+
+
+        case "SpotLight":
+            const spotLight = new THREE.SpotLight(0xffffff);
+            spotLight.position.set(100, 1000, 100);
+            spotLight.map = new THREE.TextureLoader().load(url);
+
             spotLight.castShadow = true;
-            
+
             spotLight.shadow.mapSize.width = 1024;
             spotLight.shadow.mapSize.height = 1024;
-            
+
             spotLight.shadow.camera.near = 500;
             spotLight.shadow.camera.far = 4000;
             spotLight.shadow.camera.fov = 30;
-            
-            scene.add( spotLight );
-         break;
+
+            scene.add(spotLight);
+            break;
         default:
 
-      }
+    }
 
 
 }
